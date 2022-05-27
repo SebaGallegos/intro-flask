4c4152805cc3613ad0d2ffc14dc7d542dd23407c
SebaGallegos <sebagallegos2@gmail.com>
2022-05-26 22:51:07 -0400

    commit inicial

---
+++ init.py
@@ -0,0 +1,10 @@
+# Init file to execute app
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hola mundo"
+
+if __name__ == "__main__":
+    app.run(debug=True)
