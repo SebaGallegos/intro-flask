695b1da3bc58309a31e8e9f4f6ed0dbb79322217
SebaGallegos <sebagallegos2@gmail.com>
2022-05-29 19:42:15 -0400

    1er renderizado de archivos desde flask

--- init.py
+++ init.py
@@ -1,10 +1,17 @@
 # Init file to execute app
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hola mundo"
+
+# example with bootstrap in a html file
+@app.route("/1")
+def firstpage():
+    return render_template('1/index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
