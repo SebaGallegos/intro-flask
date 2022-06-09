ec5eaced95f0a5272836ce41f9f2f587bc337b6f
SebaGallegos <sebagallegos2@gmail.com>
2022-06-09 14:33:54 -0400

    Gestion de error 404

--- init.py
+++ init.py
@@ -1,21 +1,33 @@
-# Init file to execute app
-from flask import Flask
-from flask import render_template
+# Archivo init.py - archivo de inicio de Flask
+
+# modulos
+from flask import Flask # -> modulo principal
+from flask import render_template # -> modulo para renderizado de archivos html
+from flask import abort, redirect # -> redirecciones y errores
 
 app = Flask(__name__)
 
+# error 404
+@app.errorhandler
+def pagina_no_encontrada(error):
+    return render_template("pag_no_encontrada.html"), 404
+
+# pagina simple en la ruta raiz
 @app.route("/")
 def home():
     return "Hola mundo"
 
-# example with bootstrap in a html file
+# ejemplo de renderizado de paginas html
 @app.route("/1")
 def firstpage():
     return render_template('1/index.html')
 
+# ejemplo de renderizado de paginas html
 @app.route("/2")
 def secondpage():
     return render_template('2/index.html')
 
+
+# para ejecutar el codigo
 if __name__ == "__main__":
     app.run(debug=True)
